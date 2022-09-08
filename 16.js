f197509067ef1346afa129842b24dcf04d665af2
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-08 23:42:21 +0600

    Add files via upload

---
+++ 16.js
@@ -0,0 +1,15 @@
+var letter = prompt("Enter the letter :");
+
+
+letter = letter.toLowerCase();
+
+if(letter=="a"|| letter=="e"|| letter=="i"|| letter=="o"|| letter=="u"){
+    document.write("Vowel");
+}
+    
+
+
+else{
+    document.write("consonent");
+}
+  
